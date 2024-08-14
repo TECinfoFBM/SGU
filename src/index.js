1651b7b825908b54edf6bcf9eff8b6bcb40513a9
profcosta <113833647+profcosta@users.noreply.github.com>
2024-08-14 10:38:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const ip = require("ip").address();
 const protocol = process.env.PROTOCOL || "http"
 const port = process.env.PORT || 3000
 
+//Configuração dos arquivos publicos e estáticos
+const path = require("path");
+app.use(express.static(path.join(__dirname, '../public')));
+
 //Configuração do Body-Parser
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -19,9 +23,7 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-//Configuração dos arquivos publicos e estáticos
-const path = require("path");
-app.use(express.static(path.join(__dirname, '../public')));
+
 
 app.listen(port, () => {
     console.log(`Server start in: http://localhost:${port}`)
